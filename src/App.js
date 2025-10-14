09c42ac8cea78f01ada8794e813f69d2cc70e1f7
noe <noexd2244@gmail.com>
2025-10-14 18:36:45 +0200

    Primer commit de react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import MiLista from './milista';
+import Header from './header/header';
+import Footer from './footer/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+
+      <h1>Hola mundo</h1>
+      <h2>Este es mi primer componente React</h2>
+      <div className="parrafo">
+        <p>Bienvenido a mi aplicación, esto fue creado con Javascript en React</p>
+      </div>
+
+      <br />
+
+      <MiLista
+        titulo="Profesores"
+        nombre1="Claudia"
+        nombre2="Severino"
+        nombre3="Lourdes"
+      />
+
+      <br />
+
+      <MiLista
+        titulo="Amigos"
+        nombre1="Romina"
+        nombre2="Deva"
+        nombre3="Roberto"
+        nombre4="Christian"
+      />
+
+      <br />
+
+      <MiLista
+        titulo="Familiares"
+        nombre1="Ana"
+      />
+
+      <Footer />
     </div>
   );
 }
