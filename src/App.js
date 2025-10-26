1d2f30d6396c7f223e38851f2d20ad777e577eab
noe <noexd2244@gmail.com>
2025-10-26 14:58:47 +0100

    Agregar cambios para UT03_ACT1

--- src/App.js
+++ src/App.js
@@ -6,43 +6,15 @@ import Footer from './footer/footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-
-      <h1>Hola mundo</h1>
-      <h2>Este es mi primer componente React</h2>
-      <div className="parrafo">
-        <p>Bienvenido a mi aplicación, esto fue creado con Javascript en React</p>
+    <>
+      <Header/>
+      <h2>Mi app</h2>
+      <div id="parrafo">
+        <p>Bienvenido a mi app</p>
       </div>
-
-      <br />
-
-      <MiLista
-        titulo="Profesores"
-        nombre1="Claudia"
-        nombre2="Severino"
-        nombre3="Lourdes"
-      />
-
-      <br />
-
-      <MiLista
-        titulo="Amigos"
-        nombre1="Romina"
-        nombre2="Deva"
-        nombre3="Roberto"
-        nombre4="Christian"
-      />
-
-      <br />
-
-      <MiLista
-        titulo="Familiares"
-        nombre1="Ana"
-      />
-
-      <Footer />
-    </div>
+      <MiLista/>
+      <Footer/>
+    </>
   );
 }
 
